f326de517c774243c4329db3523e2cc6a0d4355d
Dorachima <dcchris006gmail.com>
2026-05-22 21:11:27 +0100

    Complete homepage design

---
+++ home.js
@@ -0,0 +1,66 @@
+const hamburger = document.getElementById("hamburger");
+const sideMenu = document.getElementById("sideMenu");
+const closeBtn = document.getElementById("closeBtn");
+
+hamburger.addEventListener("click", () => {
+    sideMenu.classList.add("show");
+});
+
+closeBtn.addEventListener("click", () => {
+    sideMenu.classList.remove("show");
+});
+
+// home.js
+
+const texts = [
+    "Digital Experiences",
+    "Modern Websites",
+    "Branding Solutions",
+    "Mobile Applications"
+];
+
+let index = 0;
+
+const changingText = document.getElementById("changing-text");
+
+setInterval(() => {
+
+    changingText.style.opacity = "0";
+
+    setTimeout(() => {
+
+        index++;
+
+        if(index >= texts.length){
+            index = 0;
+        }
+
+        changingText.textContent = texts[index];
+
+        changingText.style.opacity = "1";
+
+    }, 500);
+
+}, 3000);
+
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+
+    reveals.forEach((section) => {
+
+        const windowHeight = window.innerHeight;
+
+        const sectionTop = section.getBoundingClientRect().top;
+
+        const revealPoint = 150;
+
+        if(sectionTop < windowHeight - revealPoint){
+
+            section.classList.add("active");
+
+        }
+
+    });
+
+});
