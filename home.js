3fac882fbac337f6b21098d6df09866800d87da0
Dorachima <dcchris006gmail.com>
2026-05-23 14:59:35 +0100

    Complete services page

--- home.js
+++ home.js
@@ -64,3 +64,13 @@ window.addEventListener("scroll", () => {
     });
 
 });
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+    const question = item.querySelector(".faq-question");
+
+    question.addEventListener("click", () => {
+        item.classList.toggle("active");
+    });
+});
